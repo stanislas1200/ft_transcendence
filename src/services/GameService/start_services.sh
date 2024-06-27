cb1a8b1d63e037ce6dfd891ea6636ba4080c5070
stanislas1200 <34397853+stanislas1200@users.noreply.github.com>
2024-06-27 13:17:53 +0200

    Fix: db gametype insert for gamestats

--- src/services/GameService/start_services.sh
+++ src/services/GameService/start_services.sh
@@ -2,6 +2,7 @@
 service redis-server start
 
 python3 manage.py migrate
-psql -h db -U postgres -d postgres -c "INSERT INTO \"PongGame_gametype\" (name) VALUES ('pong');"
+psql -h db -U postgres -d postgres -tc "SELECT 1 FROM pg_constraint WHERE conname = 'unique_name'" | grep -q 1 || psql -h db -U postgres -d postgres -c "ALTER TABLE \"PongGame_gametype\" ADD CONSTRAINT unique_name UNIQUE (name);"
+psql -h db -U postgres -d postgres -c "INSERT INTO \"PongGame_gametype\" (name) VALUES ('pong') ON CONFLICT (name) DO NOTHING;"
 
 daphne -p 8001 GameService.asgi:application -b 0.0.0.0
