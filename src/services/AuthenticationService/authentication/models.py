ea1a2968866d2ffae2b579d801aae05cbf5d4484
sgodin <sgodin@student.s19.be>
2024-07-26 16:23:00 +0200

    💤

--- src/services/AuthenticationService/authentication/models.py
+++ src/services/AuthenticationService/authentication/models.py
@@ -5,7 +5,7 @@ import os
 def user_directory_path(instance, filename):
     return os.path.join('user_{0}/avatar/'.format(instance.user.id), filename)
 
-class UserToken(models.Model):
+class UserToken(models.Model): # TODO : Social service ?: use ws to get online/offline +- and for notification db notif on connect send all if read delete from db. Add friends
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     token = models.CharField(max_length=255)
     avatar = models.ImageField(upload_to=user_directory_path, blank=True, null=True)
