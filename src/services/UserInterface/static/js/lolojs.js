4bee21dcfd0b2c68dd47821625d5e15ede9434d2
Noan Loupe <noloupe@fu-r8-p8.s19.be>
2024-08-28 13:19:09 +0200

    fixed js

--- src/services/UserInterface/static/js/lolojs.js
+++ src/services/UserInterface/static/js/lolojs.js
@@ -1,28 +1,36 @@
-const inputs = document.querySelectorAll('.input');
-const button = document.querySelector('.login__button');
+document.addEventListener('DOMContentLoaded', () => {
+  const inputs = document.querySelectorAll('.input');
+  const button = document.querySelector('.login__button');
 
-const handleFocus = ({ target }) => {
-  const span = target.previousElementSibling;
-  span.classList.add('span-active');
-}
-
-const handleFocusOut = ({ target }) => {
-  if (target.value === '') {
-    const span = target.previousElementSibling;
-    span.classList.remove('span-active');
+  const handleFocus = ({ target }) => {
+      const span = target.previousElementSibling;
+      if (span) {
+          span.classList.add('span-active');
+      }
   }
-}
 
-const handleChange = () => {
-  const [username, password] = inputs;
-
-  if (username.value && password.value.length >= 8) {
-    button.removeAttribute('disabled');
-  } else {
-    button.setAttribute('disabled', '');
+  const handleFocusOut = ({ target }) => {
+      if (target.value === '') {
+          const span = target.previousElementSibling;
+          if (span) {
+              span.classList.remove('span-active');
+          }
+      }
   }
-}
 
-inputs.forEach((input) => input.addEventListener('focus', handleFocus));
-inputs.forEach((input) => input.addEventListener('focusout', handleFocusOut));
-inputs.forEach((input) => input.addEventListener('input', handleChange));
+  const handleChange = () => {
+      const [username, password] = inputs;
+
+      if (username.value && password.value.length >= 8) {
+          button.removeAttribute('disabled');
+      } else {
+          button.setAttribute('disabled', '');
+      }
+  }
+
+  inputs.forEach((input) => {
+      input.addEventListener('focus', handleFocus);
+      input.addEventListener('blur', handleFocusOut);
+      input.addEventListener('input', handleChange);
+  });
+});
