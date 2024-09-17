16093eb12d14a06107e1884236c52fa6fb427ae8
Guillaume Duchesne <gduchesn@fu-c3-p4.s19.be>
2024-09-17 18:48:01 +0200

    websocket working ! add redis-server add chat working !!

--- src/services/Tchat/Tchat/consumers.py
+++ src/services/Tchat/Tchat/consumers.py
@@ -5,14 +5,32 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class TChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.chat_group_name = f'chat_1'
+
+        await self.channel_layer.group_add(
+            self.chat_group_name,
+            self.channel_name
+        )
+        print("yo", flush=True)
         await self.accept()
 
     async def disconnect(self, close_code):
         pass
 
+    async def update_message_state(self, event):
+        await self.send(text_data=json.dumps(event['message']))
+
     async def receive(self, text_data):
+        print("In receive", flush=True)
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-        await self.send(text_data=json.dumps({
-            'message': message
-        }))
+        await self.channel_layer.group_send(
+            self.chat_group_name,
+            {
+                'type': 'update_message_state',
+                'message': text_data_json
+            }
+        )
+        # await self.send(text_data=json.dumps({
+        #     'message': message
+        # }))
--- src/services/Tchat/Tchat/routing.py
+++ src/services/Tchat/Tchat/routing.py
@@ -1,8 +1,7 @@
-# Tchat/routing.py
-
+# routing.py
 from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/chat/$', consumers.TChatConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.TChatConsumer.as_asgi()),
 ]
