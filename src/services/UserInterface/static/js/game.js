e1b78f5133a0c5568e2078b12ad58970025eca30
Bryan Minner <brminner@mi-c1-p1.s19.be>
2024-09-24 17:47:01 +0200

    finish with game.js next stop to pong.js

--- src/services/UserInterface/static/js/game.js
+++ src/services/UserInterface/static/js/game.js
@@ -1,49 +1,51 @@
-let maxPlayersSelectRandom;
-let maxPlayersSelectCreate;
+// let maxPlayersSelectRandom;
+// let maxPlayersSelectCreate;
 // let gameModeSelect;
-let gameModeSelectCreate;
+// let gameModeSelectCreate;
 // let joinGameButton;
 // let joinGameName;
 // let randomGameButton;
 let loadingOverlay;
 let loadingText;
 let playerPositions;
-let createGameButton;
+// let createGameButton;
 // let gameName;
-let gameMode;
-let mapChoice;
-let ballSpeed;
-let paddleSpeed;
+// let gameMode;
+// let mapChoice;
+// let ballSpeed;
+// let paddleSpeed;
 
 function getElementGame() {
     console.log('game');
-    maxPlayersSelectRandom = document.getElementById('max-players-random');
-    maxPlayersSelectCreate = document.getElementById('max-players-create');
+    // maxPlayersSelectRandom = document.getElementById('max-players-random');
+    // maxPlayersSelectCreate = document.getElementById('max-players-create');
     // gameModeSelect = document.getElementById('game-mode');
     // gameModeSelect.disabled = true;
-    gameModeSelectCreate = document.getElementById('game-mode-create');
+    // gameModeSelectCreate = document.getElementById('game-mode-create');
     // joinGameButton = document.getElementById('join-game-button');
     // joinGameName = document.getElementById('join-game-name').value;
     // randomGameButton = document.getElementById('random-game-button');
     loadingOverlay = document.getElementById('loading-overlay');
     loadingText = document.getElementById('loading-text');
     playerPositions = document.getElementById('player-positions');
-    createGameButton = document.getElementById('create-game-button');
+    // createGameButton = document.getElementById('create-game-button');
 
     // gameName = document.getElementById('game-name').value;
-    gameMode = document.getElementById('game-mode').value;
-    mapChoice = document.getElementById('map-choice').value;
-    ballSpeed = parseInt(document.getElementById('ball-speed').value);
-    paddleSpeed = parseInt(document.getElementById('paddle-speed').value);
+    // gameMode = document.getElementById('game-mode').value;
+    // mapChoice = document.getElementById('map-choice').value;
+    // ballSpeed = parseInt(document.getElementById('ball-speed').value);
+    // paddleSpeed = parseInt(document.getElementById('paddle-speed').value);
 
     gameModeDisabler();
     inputAnimation();
     randomJoinGameButton();
-    waitingRoom();
+    createGameButton();
+    // waitingRoom();
 }
 
 function gameModeDisabler() {
     // Désactive le choix du mode de jeu si 2 joueurs sont sélectionnés
+    const maxPlayersSelectRandom = document.getElementById('max-players-random');
     const gameModeSelect = document.getElementById('game-mode');
     gameModeSelect.disabled = true;
     maxPlayersSelectRandom.addEventListener('change', function () {
@@ -55,6 +57,7 @@ function gameModeDisabler() {
         }
     });
     
+    const maxPlayersSelectCreate = document.getElementById('max-players-create');
     const gameModeCreateTeam = document.getElementById('game-mode-create-team');
     const gameModeCreateFfa = document.getElementById('game-mode-create-ffa');
     const gameModeCreateSolo = document.getElementById('game-mode-create-solo');
@@ -83,10 +86,11 @@ function gameModeDisabler() {
 function randomJoinGameButton() {
     const randomGameButton = document.getElementById('random-game-button');
     const gameModeSelect = document.getElementById('game-mode');
+    const maxPlayersSelectRandom = document.getElementById('max-players-random');
 
     randomGameButton.addEventListener('click', function () {
         var xhr = new XMLHttpRequest();
-        url = window.location.href + "/game/join?gameName=pong&gameMode=" + gameModeSelect.value;
+        var url = "https://" + window.location.href + ":8001/game/join?gameName=pong&gameMode=" + gameModeSelect.value + "&nbPlayers=" + maxPlayersSelectRandom.value;
         xhr.withCredentials = true;
         xhr.open("GET", url, true);
         xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
@@ -94,55 +98,89 @@ function randomJoinGameButton() {
             if (xhr.readyState === 4)
                 if (xhr.status === 200) {
                     console.log('Game joined');
+                    console.log(xhr.responseText);
                 }
                 else {
                     console.log('Error joining game'); // TODO put a message
+                    console.log(xhr.responseText);
                 }
         };
         xhr.send();
     });
 }
 
-function waitingRoom() {
-    // Gestion de la création de partie
+function createGameButton() {
+    const createGameButton = document.getElementById('create-game-button');
+
     createGameButton.addEventListener('click', function () {
-        // console.log('je suis occupe de creer une game!');
-        const maxPlayers = maxPlayersSelectCreate.value;
-        gameName = document.getElementById('game-name').value;
-        if (!gameName) {
-            alert("Veuillez entrer un nom pour la partie.");
-            return;
+        const partyName = document.getElementById('partyName').value;
+        const playerNumber = document.getElementById('max-players-create').value;
+        const gameMode = document.getElementById('game-mode-create').value;
+        const mapChoice = document.getElementById('map-choice').value;
+        const ballSpeed = document.getElementById('ball-speed').value;
+        const paddleSpeed = document.getElementById('paddle-speed').value;
+    
+        var xhr = new XMLHttpRequest();
+        let url = "https://" + window.location.hostname + ":8001" + "/game/create";
+        console.log(url);
+        xhr.withCredentials = true;
+        xhr.open("POST", url, true);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4)
+                if (xhr.status === 200) {
+                    console.log('Game created');
+                    console.log(xhr.responseText);
+                }
+                else {
+                    console.log('Error creating game'); // TODO put a message
+                    console.log(xhr.responseText);
+                }
         }
-
-        ballSpeed = document.getElementById('ball-speed').value;
-        // Validation des valeurs pour ballSpeed et paddleSpeed
-        if (isNaN(ballSpeed) || ballSpeed < 1 || ballSpeed > 50) {
-            alert("La vitesse de la balle doit être un nombre entre 1 et 50.");
-            return;
-        }
-
-        paddleSpeed = document.getElementById('paddle-speed').value;
-        if (isNaN(paddleSpeed) || paddleSpeed < 10 || paddleSpeed > 100) {
-            alert("La vitesse des raquettes doit être un nombre entre 10 et 100.");
-            return;
-        }
-
-        // Créez un objet de configuration de jeu avec les valeurs saisies
-        const gameConfig = {
-            gameName: gameName,
-            maxPlayers: parseInt(maxPlayers),
-            gameMode: gameMode,
-            map: parseInt(mapChoice),
-            ballSpeed: ballSpeed,
-            paddleSpeed: paddleSpeed
-        };
-
-        console.log("Création d'une partie avec les paramètres suivants:", gameConfig);
-
-        // Simulez la création de la partie (intégration backend nécessaire)
-        alert("Partie créée avec succès !");
+        xhr.send("partyName=" + partyName + "&game=pong&gameType=custom&playerNumber=" + playerNumber + "&gameMode=" + gameMode + "&map=" + mapChoice + "&ballSpeed=" + ballSpeed + "&paddleSpeed=" + paddleSpeed);
     });
 }
+
+// function waitingRoom() {
+//     // Gestion de la création de partie
+//     createGameButton.addEventListener('click', function () {
+//         // console.log('je suis occupe de creer une game!');
+//         const maxPlayers = maxPlayersSelectCreate.value;
+//         gameName = document.getElementById('game-name').value;
+//         if (!gameName) {
+//             alert("Veuillez entrer un nom pour la partie.");
+//             return;
+//         }
+
+//         ballSpeed = document.getElementById('ball-speed').value;
+//         // Validation des valeurs pour ballSpeed et paddleSpeed
+//         if (isNaN(ballSpeed) || ballSpeed < 1 || ballSpeed > 50) {
+//             alert("La vitesse de la balle doit être un nombre entre 1 et 50.");
+//             return;
+//         }
+
+//         paddleSpeed = document.getElementById('paddle-speed').value;
+//         if (isNaN(paddleSpeed) || paddleSpeed < 10 || paddleSpeed > 100) {
+//             alert("La vitesse des raquettes doit être un nombre entre 10 et 100.");
+//             return;
+//         }
+
+//         // Créez un objet de configuration de jeu avec les valeurs saisies
+//         const gameConfig = {
+//             gameName: gameName,
+//             maxPlayers: parseInt(maxPlayers),
+//             gameMode: gameMode,
+//             map: parseInt(mapChoice),
+//             ballSpeed: ballSpeed,
+//             paddleSpeed: paddleSpeed
+//         };
+
+//         console.log("Création d'une partie avec les paramètres suivants:", gameConfig);
+
+//         // Simulez la création de la partie (intégration backend nécessaire)
+//         alert("Partie créée avec succès !");
+//     });
+// }
 
 function inputAnimation() {
     const inputs = document.querySelectorAll('.input');
