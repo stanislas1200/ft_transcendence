d3a040face00820bd1b928fab9ff230ff6707fde
Hugo Brygo <hubrygo@fu-c3-p4.s19.be>
2024-09-25 18:30:06 +0200

    modif error des settings

--- src/services/UserInterface/static/js/profile.js
+++ src/services/UserInterface/static/js/profile.js
@@ -1,5 +1,6 @@
 function loadProfile() {
     const userName = document.getElementById('userName');
+    console.log('userName');
     var response;
     let url = "https://localhost:8000/me";
     url = url.replace("localhost", window.location.hostname); var xhr = new XMLHttpRequest();
@@ -10,12 +11,12 @@ function loadProfile() {
         if (xhr.readyState === 4) {
             if (xhr.status === 200 || xhr.status === 201) {
                 response = JSON.parse(xhr.responseText);
+                userName.textContent = response.username;
+                loadProfilePicture(response.id);
             } else {
                 alert('Error: ' + JSON.parse(xhr.responseText).error);
             }
         }
-        userName.textContent = response.username;
-        loadProfilePicture(response.id);
     };
     xhr.send();
 }
